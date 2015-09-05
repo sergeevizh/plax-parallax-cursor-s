1c3676e108b751c10b37a7060bf5df71d0ee8a01
yumashev <a.yumashev@gmail.com>
2015-09-05 11:21:02 +0500

    add files

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Parallax.js for WordPress by Systemo
+Plugin URI: ${TM_PLUGIN_BASE}
+Description: Создает Параллакс эффект от курсора шорткодом типа: [plax-s][plaxify][plaxify x=20 y=33 invert=true selector=".btn"][/plax-s]
+Author: ${TM_NAME}
+Author URI: ${TM_HOMEPAGE}
+Version: 0.1
+*/
+
+
+
+function load_ss_plax_s(){
+
+  wp_register_script( 'plax', plugins_url( '/inc/plax/js/plax.js',__FILE__ ), array('jquery'), '1');
+  wp_enqueue_script('plax');
+
+} add_action('wp_enqueue_scripts', 'load_ss_plax_s');
+
+
+
+add_shortcode( 'plax-s', function($atts, $content){
+
+  ob_start();
+  ?>
+  <script type="text/javascript">
+
+    (function ($) {
+      <?php echo do_shortcode( $content ); ?>
+
+      $.plax.enable();
+    }(jQuery));
+  </script>
+
+  <?php
+  $html = ob_get_contents();
+  ob_get_clean();
+
+  return $html;
+
+});
+
+add_shortcode( 'plaxify', function($atts){
+  extract( shortcode_atts( array(
+    'selector' => "img",
+    'x' => 22,
+    'y' => 33,
+    'invert' => 'false',
+  ), $atts ));
+
+  $parametrs = '"xRange": '.$x . ', "yRange": '.$y . ', "invert": ' . $invert;
+
+  return '$("' . $selector . '").plaxify({' . $parametrs . '});';
+});
